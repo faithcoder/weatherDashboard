0fa6ef1f921e4a6e9bbf1fb8d708c28fe9d94577
Abdullah Al Arif <48954386+faithcoder@users.noreply.github.com>
2024-02-10 23:48:08 +0600

    api and custom hook added

---
+++ src/components/weather/WeatherBoard.jsx
@@ -0,0 +1,22 @@
+import { useWeather } from "../../hooks";
+import AddToFavourite from "./AddToFavourite";
+import WeatherCondtions from "./WeatherCondition";
+import WeatherHeadline from "./WeatherHeadline";
+
+export default function WeatherBoard() {
+  const { loading, error, weatherData } = useWeather;
+  console.log(weatherData, loading, error);
+  return (
+    <section className="">
+      <div className="container">
+        <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
+          <div className="grid md:grid-cols-2 gap-10 md:gap-6">
+            <AddToFavourite />
+            <WeatherHeadline />
+            <WeatherCondtions />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/hooks/useWeather.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+
+const useWeather = () => {
+  const [weather, setWeather] = useState({
+    location: "",
+    climate: "",
+    temperature: "",
+    maxTemperature: "",
+    minTemperature: "",
+    humidity: "",
+    cloudPercentage: "",
+    wind: "",
+    time: "",
+    longitude: "",
+    latitude: "",
+  });
+  
+
+  const [loading, setLoading] = useState({
+    state: false,
+    message: "",
+  });
+
+  const [error, setError] = useState(null);
+
+  const fetchWeatherData = async (latitude, longitude) => {
+    try {
+      setLoading({
+        ...loading,
+        state: true,
+        message: "Loading...",
+      });
+
+      const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${latitude}&lon=${longitude}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`);
+      if (!response.ok) {
+        const errorMessage = `Fetcing weather data failed: ${response.status}`;
+        throw new Error(errorMessage);
+      }
+
+      const data = await response.json();
+      const updateWeatherData = {
+        location: data?.name,
+        climate: data?.weather[0]?.main,
+        temperature: data?.main?.temp,
+        maxTemperature: data?.main?.temp_max,
+        minTemperature: data?.main?.temp_min,
+        humidity: data?.main?.humidity,
+        cloudPercentage: data?.clouds?.all,
+        wind: data?.wind?.speed,
+        time: data?.dt,
+        longitude: longitude,
+        latitude: latitude,
+      }
+      setWeather(updateWeatherData);
+
+  } catch (error) {
+    setError(error);
+  } finally {
+    setLoading({
+      ...loading,
+        state: false,
+        message: "",
+      });
+  };
+  useEffect(() => {
+    setLoading({
+        loading: true,
+        message: "Finding your location...",
+    })
+    navigator.geolocation.getCurrentPosition(function (position) => {
+        fetchWeatherData(position.coords.latitude, position.coords.longitude);
+    })
+    
+  },[]);
+  return {
+    weather,
+    loading,
+    error,
+  }
+};
+
+export default useWeather;
